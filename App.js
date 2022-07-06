ffe95fe56ee918a7c30cdfd04a24bce97361d726
rileymiranda7 <rileymiranda7@gmail.com>
2022-07-05 22:55:49 -0400

    basic screen setup started. Planning on nesting profile settings and exercises screens in stack nav in profile screen

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import StartWorkoutScreen from "./screens/StartWorkoutScreen";
+import PastWorkoutsScreen from "./screens/PastWorkoutsScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import CurrentWorkoutScreen from "./screens/CurrentWorkoutScreen";
+import "react-native-gesture-handler";
+
+const BottomTabs = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="StartWorkout"
+        component={StartWorkoutScreen}
+        options={{
+          title: "Start Workout",
+        }}
+      />
+      <Drawer.Screen name="Profile" component={ProfileScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>RyFit</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <BottomTabs.Navigator initialRouteName="Drawer">
+          <BottomTabs.Screen
+            name="PastWorkouts"
+            component={PastWorkoutsScreen}
+            options={{
+              title: "Past Workouts",
+            }}
+          />
+          <BottomTabs.Screen
+            name="Drawer"
+            component={DrawerNavigator}
+            options={{
+              headerShown: false,
+              title: "Start Workout",
+            }}
+          />
+          <BottomTabs.Screen
+            name="CurrentWorkout"
+            component={CurrentWorkoutScreen}
+            options={{
+              title: "Current Workout",
+            }}
+          />
+        </BottomTabs.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
